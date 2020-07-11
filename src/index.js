c6395be10fd4ee19ab00d1cb5ad9355b544a9b06
its-a-wrap <patrick_tweed@hotmail.co.uk>
2020-07-11 14:03:02 +0100

    first iteration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-export const placeHolder = () => {
-    return 0;
+export function formatDuration (seconds) {
+    if (!seconds) return 'now';
+    let remainingTime = seconds;
+    let stringSections = [];
+
+    Object.keys(secondsToUnitOfTime).forEach(unit => {
+        remainingTime = formatUnitOfTime(remainingTime, stringSections, unit);
+    })
+
+    return turnSectionsIntoString(stringSections)
+}
+
+function formatUnitOfTime(remainingTime, stringSections, unit) {
+    if (remainingTime >= secondsToUnitOfTime[unit]) {
+        const numOfUnitOfTime = Math.floor(remainingTime / secondsToUnitOfTime[unit]);
+        remainingTime -= (numOfUnitOfTime * secondsToUnitOfTime[unit]);
+        numOfUnitOfTime === 1 ? stringSections.push(`1 ${unit}`) : stringSections.push(`${numOfUnitOfTime} ${unit}s`)
+    }
+
+    return remainingTime;
+}
+
+function turnSectionsIntoString(sections) {
+    let formattedString = '';
+
+    if (sections.length === 1) {
+        return sections[0];
+    }
+    sections.forEach((section, index, arr) => {
+       if (index < arr.length -2) {
+            formattedString += section + ', '
+       } else if (index === arr.length -1) {
+            formattedString += 'and ' + section;
+       } else if (index === arr.length - 2) {
+           formattedString += section + ' ';
+       }
+    });
+
+    return formattedString;
+}
+
+const secondsToUnitOfTime = {
+    'year': 31536000,
+    'day': 86400,
+    'hour': 3600,
+    'minute': 60,
+    'second': 1
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,10 +1,36 @@
-import {placeHolder} from './index.js';
+import {formatDuration} from './index.js';
 import chai from 'chai';
 
 const {expect} = chai;
 
-describe('some test', () => {
-    it('SHOULD do something', () => {
-        expect(placeHolder()).to.be.eql(0);
+describe('GIVEN formatDuration function', () => {
+    describe(`AND the input is '1'`, () => {
+        it('SHOULD return `1 second`', () => {
+            expect(formatDuration(1)).to.be.eql('1 second');
+        });
+    })
+
+    describe(`AND the input is '62'`, () => {
+        it('SHOULD return `1 minute and 2 seconds`', () => {
+            expect(formatDuration(62)).to.be.eql('1 minute and 2 seconds');
+        });
+    });
+
+    describe(`AND the input is '120'`, () => {
+        it('SHOULD return `2 minutes`', () => {
+            expect(formatDuration(120)).to.be.eql('2 minutes');
+        });
+    });
+
+    describe(`AND the input is '3600'`, () => {
+        it('SHOULD return `1 hour', () => {
+            expect(formatDuration(3600)).to.be.eql('1 hour');
+        });
+    });
+
+    describe(`AND the input is '3662'`, () => {
+        it('SHOULD return `1 hour, 1 minute and 2 seconds', () => {
+            expect(formatDuration(3662)).to.be.eql('1 hour, 1 minute and 2 seconds');
+        });
     });
 });
